cd9ad7b12f4c30a5281b5086592a7fbf32394c48
hasan1119 <contact@rokibul.com>
2023-05-31 22:34:54 +0600

    ok

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+
+const typed =  new Typed('.typed', {
+    strings: ["Web Developer", "Web Designer", "Full Stack Web Developer", "Freelancer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+    cursorBlinking:true,
+  });
+
